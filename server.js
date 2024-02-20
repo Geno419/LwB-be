d151e29609ce0f745d43512e82658c4f9d186685
Sana <khalifa.sana@gmail.com>
2024-02-20 09:27:36 +0000

    video upload

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const cors = require("cors");
 const studentRouter = require("./Routes/student");
 const broadcastRouter = require("./Routes/broadcast");
 const viewerRouter = require("./Routes/viewer");
-
+const path = require("path");
+const multer = require("multer");
+const {GridFsStorage} = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
 const bodyParser = require('body-parser');
 
 
@@ -27,6 +30,24 @@ async function connect() {
 }
 connect();
 app.use(express.json());
+
+// Set up GridFS storage engine
+const storage = new GridFsStorage({
+  url: uri,
+  file: (req, file) => {
+    return {
+      filename: file.originalname
+    };
+  }
+});
+const upload = multer({ storage });
+
+// Route to handle file uploads
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log("File uploaded successfully:", req.file);
+  const fl= req.file;
+  res.send({file: fl, success: true, message: 'File uploaded successfully' });
+});
 
 app.use("/students", studentRouter);
 
