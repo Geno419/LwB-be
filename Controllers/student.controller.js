ca6b36a626ce52bd8b32fe4becd81779d918d95a
sawm2000 <sabreenmaflahi@outlook.com>
2024-02-27 18:13:14 +0000

    password update

--- Controllers/student.controller.js
+++ Controllers/student.controller.js
@@ -1,13 +1,18 @@
-const createError = require("../error.js")
-const Student = require ("../Models/student.model.js")
+const Student = require("../Models/student.model.js");
+const bcrypt = require("bcrypt");
 
 const update = async (req, res, next) => {
- 
-try {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.userPassword, salt);
+
+  try {
     const updatedUser = await Student.findByIdAndUpdate(
       req.params.id,
       {
-        $set: req.body,
+        $set: {
+          ...req.body,
+          userPassword: hash,
+        },
       },
       { new: true }
     );
@@ -15,26 +20,24 @@ try {
   } catch (err) {
     next(err);
   }
-}
-
+};
 
 const deleteUser = async (req, res, next) => {
-
   try {
     await Student.findByIdAndDelete(req.params.id);
     res.status(200).json("Student has been deleted.");
   } catch (err) {
     next(err);
   }
-}
+};
 
 const getUser = async (req, res, next) => {
-try {
-  const user = await Student.findById(req.params.id);
-  res.status(200).json(user);
-} catch (err) {
-  next(err);
-}
+  try {
+    const user = await Student.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const getAllUsers = async (req, res, next) => {
@@ -44,6 +47,6 @@ const getAllUsers = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-  };
+};
 
-module.exports = { getUser, deleteUser, update, getAllUsers}
+module.exports = { getUser, deleteUser, update, getAllUsers };
